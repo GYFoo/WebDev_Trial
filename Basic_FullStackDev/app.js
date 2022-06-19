0ed8bfd91088837e40e77b52ed786d4f06ae6112
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-06-19 18:44:30 +0800

    added and tested codes for /users route

--- Basic_FullStackDev/app.js
+++ Basic_FullStackDev/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const { UserExistsError } = require('./managers/error');
 const app = express();
 const { now } = require('./managers/time');
+const userManager = require('./managers/users');
+const createHttpError = require('http-errors');
+const bp = require('body-parser');
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 // basic route
 app.get('/', (req, res) => {
@@ -11,7 +17,34 @@ app.get('/', (req, res) => {
 app.get('/now', (req, res) => {
     now().then((time) => {
         return res.json({ now: time });
-    })
+    });
+});
+
+// create a new user route
+app.post('/users', (req, res, next) => {
+    // const username = req.body.username;
+    // const password = req.body.password;
+    // destructuring
+    const { username, password } = req.body;
+    // send to the database
+    userManager.create(username, password)
+        .then(() => {
+            return res.sendStatus(201);
+        })
+        .catch((error) => {
+            if (error instanceof UserExistsError) {
+                return next(createHttpError(409, error.message));
+            }
+            else {
+                return next(error);
+            }
+        });
+});
+
+// error handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    return res.status(error.status || 500).json({ "error": error.message || 'Unknown Error!' });
 });
 
 module.exports = app;
