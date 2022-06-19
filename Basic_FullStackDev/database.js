9143e2c3975f063930cba0c110fb815343032baa
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-06-19 15:43:02 +0800

    established connection to database

---
+++ Basic_FullStackDev/database.js
@@ -0,0 +1,27 @@
+// handles database connection
+require('dotenv').config();
+const pg = require('pg');
+
+// manages a pool of connection
+const pool = new pg.Pool ({
+    // url to the database
+    connectionString: process.env.DATABASE_URL,
+    // max no. of connections in this pool
+    max: 5,
+    // secure socket layer
+    ssl: {
+        rejectUnauthorized: true,
+    },
+});
+
+// for testing
+// pool.query('SELECT NOW()')
+//     .then((response) => {
+//         console.log(response.rows[0].now);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+// export the pool
+module.exports = pool;
