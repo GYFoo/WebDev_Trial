d3fe3f267b43cf4a1fc61449ee98d879848652d9
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-07-04 13:41:38 +0800

    initialized react app

---
+++ react-app-example/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
