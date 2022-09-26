f2abe1608635998108f14558bec36d28f45ec548
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-09-26 12:50:05 +0800

    added getuser and compare password with hash

--- Basic_FullStackDev/managers/users.js
+++ Basic_FullStackDev/managers/users.js
@@ -1,10 +1,14 @@
 const dbConn = require('../database');
 const bcrypt = require('bcrypt');
 const { POSTGRES_ERROR_CODES } = require('../database/error');
-const { UserExistsError } = require('./error');
+const { UserExistsError, NoSuchUserError, PasswordMismatchError } = require('./error');
 
 function hashPassword(password) {
     return bcrypt.hash(password, +process.env.SALT_ROUNDS);
+}
+
+function comparePassword(password, hash) {
+    return bcrypt.compare(password, hash);
 }
 
 function insertIntoDatabase(username, hash) {
@@ -26,3 +30,28 @@ function insertIntoDatabase(username, hash) {
 module.exports.create = (username, password) => {
     return hashPassword(password).then((hash) => insertIntoDatabase(username, hash));
 };
+
+function getUserByUsername(username) {
+    const query = `SELECT password FROM users WHERE username = $1`;
+    const params = [username];
+    return dbConn.query(query, params).then((response) => {
+        if (response.rows.length === 0) {
+            // no such user
+            throw new NoSuchUserError(username);
+        }
+        return response.rows[0].password;
+    });
+}
+
+module.exports.compare = (username, password) => {
+    return getUserByUsername(username)
+    .then((hash) => {
+        return comparePassword(password, hash);
+    })
+    .then((isMatched) => {
+        if (!isMatched) {
+            throw new PasswordMismatchError(username);
+        }
+        return;
+    });
+}
