9b9a2e4aff8ba00e0f8c798723f7f9d139ae830b
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-09-26 12:49:01 +0800

    added 2 new errors res

--- Basic_FullStackDev/managers/error.js
+++ Basic_FullStackDev/managers/error.js
@@ -1,5 +1,17 @@
 module.exports.UserExistsError = class UserExistsError extends Error {
     constructor(username) {
-        super(`user ${username} already exists`);
+        super(`user ${username} already exists!`);
+    }
+};
+
+module.exports.NoSuchUserError = class NoSuchUserError extends Error {
+    constructor(username) {
+        super(`username ${username} not found!`);
+    }
+};
+
+module.exports.PasswordMismatchError = class PasswordMismatchError extends Error {
+    constructor(username) {
+        super(`Wrong Password for User ${username}!`);
     }
 };
